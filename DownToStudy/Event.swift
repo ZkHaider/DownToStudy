2a99e662118a789c22dd0cadada305b7297891df
ZkHaider <hkhan@oglethorpe.edu>
2016-08-27 14:17:51 -0700

    Installed Moya

---
+++ DownToStudy/Event.swift
@@ -0,0 +1,14 @@
+//
+//  Event.swift
+//  DownToStudy
+//
+//  Created by Haider Khan on 8/27/16.
+//  Copyright © 2016 ZkHaider. All rights reserved.
+//
+
+import Foundation
+
+struct Event {
+    let id : String?
+    let name: String?
+}
---
+++ Pods/Target Support Files/Pods-DownToStudy/Pods-DownToStudy-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_DownToStudyVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_DownToStudyVersionString[];
+
