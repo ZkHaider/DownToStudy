e41468a7ff54fba18e5366b4d54a86a6b6f2f091
ZkHaider <hkhan@oglethorpe.edu>
2016-08-28 05:50:37 -0700

    Added protocol functions for message view controller

--- DownToStudy/MessageViewController.swift
+++ DownToStudy/MessageViewController.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import Chatto
+import ChattoAdditions
 
 class MessageViewController: BaseChatViewController {
     
@@ -15,13 +16,58 @@ class MessageViewController: BaseChatViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+//        setChatDataSource(self, triggeringUpdateType: UpdateType.Normal)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    override func createChatInputView() -> UIView {
+        let chatInputView = ChatInputBar.loadNib()
+        var appearance = ChatInputBarAppearance()
+        appearance.sendButtonAppearance.title = NSLocalizedString("Send", comment: "")
+        appearance.textInputAppearance.placeholderText = NSLocalizedString("Get down to study", comment: "")
+//        self.chatInputPresenter = BasicChatInputBarPresenter(chatInputBar: chatInputView, chatInputItems: self.createChatInputItems(), chatInputBarAppearance: appearance)
+        chatInputView.maxCharactersCount = 1000
+        return chatInputView
+    }
+    
+    func createChatInputItems() -> [ChatInputItemProtocol] {
+        var items = [ChatInputItemProtocol]()
+        items.append(self.createTextInputItem())
+        items.append(self.createPhotoInputItem())
+        return items
+    }
+    
+    private func createTextInputItem() -> TextChatInputItem {
+        let item = TextChatInputItem()
+        item.textInputHandler = { [weak self] text in
+            // Handling code
+        }
+        return item
+    }
+    
+    private func createPhotoInputItem() -> PhotosChatInputItem {
+        let item = PhotosChatInputItem(presentingController: self)
+        item.photoInputHandler = { [weak self] image in
+            // Handling code
+        }
+        return item
+    }
+    
+    func loadNext() {
+        
+    }
+    
+    func loadPrevious() {
+        
+    }
+    
+    func adjustNumberOfMessages(preferredMaxCount preferredMaxCount: Int?, focusPosition: Double, completion: (didAdjust: Bool) -> Void) {
+        
+    }
+    
 }
