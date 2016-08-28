30307889f4aacb7645d3664ccd19274d7f936723
Emerson Malca <emersonmalca@me.com>
2016-08-28 03:18:30 -0700

    Created delegate protocol for login view controller

--- DownToStudy/ViewController.swift
+++ DownToStudy/ViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 import Moya
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, LoginViewControllerDelegate {
     
     var viewControllers = [UIViewController]()
     var loginViewController: LoginViewController?
@@ -70,6 +70,12 @@ class ViewController: UIViewController {
     func scrollTo(page: Int, animated: Bool) {
         let xOffset = scrollView.bounds.size.width * CGFloat(page)
         scrollView.setContentOffset(CGPoint.init(x: xOffset, y: 0.0), animated: animated)
+    }
+    
+    // Login view controller delegate
+    
+    func loginDidAuthenticate(login: LoginViewController) -> Void {
+        // Remove the login controller 
     }
 }
 
